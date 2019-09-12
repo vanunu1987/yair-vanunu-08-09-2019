bc25d36744e11537034eb37af82feaca38143813
Your Name <vanunu24@gmail.com>
2019-09-12 13:11:15 +0300

    Updates

---
+++ precache-manifest.973fd4244b31c91d5fa4ce552fe62904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a826fe25cafd2729dd0e25ddbf0c2792",
+    "url": "/weather-prediction/index.html"
+  },
+  {
+    "revision": "f7385f9c7989bbccf6b8",
+    "url": "/weather-prediction/static/css/main.47324cbd.chunk.css"
+  },
+  {
+    "revision": "73739e554f6638c0dc1d",
+    "url": "/weather-prediction/static/js/2.96e7ac59.chunk.js"
+  },
+  {
+    "revision": "f7385f9c7989bbccf6b8",
+    "url": "/weather-prediction/static/js/main.f9c53e56.chunk.js"
+  },
+  {
+    "revision": "fb2b443a8ac584875e6f",
+    "url": "/weather-prediction/static/js/runtime~main.44116478.js"
+  },
+  {
+    "revision": "f99324396da378801cbf0a07ea1ae0e5",
+    "url": "/weather-prediction/static/media/dayback.f9932439.jpg"
+  },
+  {
+    "revision": "9cf6e1c1571e21dbfe0189e33c14df48",
+    "url": "/weather-prediction/static/media/nightback.9cf6e1c1.jpg"
+  }
+]);

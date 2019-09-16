e53e26c763002197742085525d5aed9ee4aa77b8
Your Name <vanunu24@gmail.com>
2019-09-16 12:14:20 +0300

    Updates

---
+++ precache-manifest.94115c3e9c0e7937f566f4035ffbb2b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ecacff806bc9eee3e779504a0019b0",
+    "url": "/yair-vanunu-08-09-2019//index.html"
+  },
+  {
+    "revision": "5e719b9f52306f92492c",
+    "url": "/yair-vanunu-08-09-2019//static/css/main.6cefbdd0.chunk.css"
+  },
+  {
+    "revision": "73739e554f6638c0dc1d",
+    "url": "/yair-vanunu-08-09-2019//static/js/2.96e7ac59.chunk.js"
+  },
+  {
+    "revision": "5e719b9f52306f92492c",
+    "url": "/yair-vanunu-08-09-2019//static/js/main.31bf894f.chunk.js"
+  },
+  {
+    "revision": "2790dde0db5f8cf099ad",
+    "url": "/yair-vanunu-08-09-2019//static/js/runtime~main.fd006539.js"
+  },
+  {
+    "revision": "f99324396da378801cbf0a07ea1ae0e5",
+    "url": "/yair-vanunu-08-09-2019//static/media/dayback.f9932439.jpg"
+  },
+  {
+    "revision": "9cf6e1c1571e21dbfe0189e33c14df48",
+    "url": "/yair-vanunu-08-09-2019//static/media/nightback.9cf6e1c1.jpg"
+  }
+]);
